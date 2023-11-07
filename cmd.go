e7b51fcf95349bad4f55308b81efc81c26f2e77f
NullpointerW <825708370@qq.com>
2023-11-07 20:29:13 +0800

    use cobra

--- cmd.go
+++ cmd.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"github.com/spf13/cobra"
 	"os"
+	"strconv"
 )
 
 var (
-	host string
-	port int
+	host    string
+	port    int
+	address string
 )
 
 var rootCmd = &cobra.Command{
@@ -23,11 +25,12 @@ var add = &cobra.Command{
 	Use:   "add",
 	Short: "Subscribe to anime series",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(args)
+
 	},
+	TraverseChildren: true,
 }
 
-var addFeed = &cobra.Command{
+var feed = &cobra.Command{
 	Use:   "feed",
 	Short: "Subscribe to anime series via rss feed",
 	Run: func(cmd *cobra.Command, args []string) {
@@ -80,12 +83,13 @@ func init() {
 	addFlags()
 	lsi.Flags().BoolVarP(&searchList, "search", "s", false, "Show search list")
 	rootCmd.AddCommand(add)
-	rootCmd.AddCommand(addFeed)
+	add.AddCommand(feed)
 	rootCmd.AddCommand(ls)
 	rootCmd.AddCommand(lsi)
 	rootCmd.AddCommand(stat)
 	rootCmd.AddCommand(rm)
 	rootCmd.AddCommand(stop)
+	address = solveAddress()
 }
 
 var (
@@ -95,6 +99,8 @@ var (
 	group      string
 	index      string
 	searchList bool
+	// add feed --name
+	infoNameForFeed string
 )
 
 func addFlags() {
@@ -103,6 +109,15 @@ func addFlags() {
 	add.Flags().BoolVarP(&useRegEXP, "regexp", "r", false, "Use regular expressions")
 	add.Flags().StringVarP(&group, "group", "g", "", "Subtitle group keywords")
 	add.Flags().StringVarP(&index, "index", "i", "", "Index of torrent list")
+	add.Flags().StringVarP(&infoNameForFeed, "name", "n", "", "KeyName for solve info on AddFeed")
+}
+
+func solveAddress() string {
+	def := "127.0.0.1:12314"
+	if port != 0 && host != "" {
+		def = host + ":" + strconv.Itoa(port)
+	}
+	return def
 }
 
 func Execute() {
--- transport.go
+++ transport.go
@@ -1,84 +1,50 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
+	"encoding/json"
+	N "github.com/NullpointerW/anicat/net"
 	// "log"
 	"net"
-	"os"
-	"os/exec"
-	"runtime"
-	"strconv"
-
-	N "github.com/NullpointerW/anicat/net"
-	"github.com/NullpointerW/anicat/net/cmd"
 )
 
-func main() {
-	Execute()
-	return
-	signal := make(chan struct{})
-	go waitProgress(signal)
+type cTyp int
 
-	r := bufio.NewReader(os.Stdin)
-	defer func() {
-		r.ReadString('\n')
-	}()
-	dialadress := host + ":" + strconv.Itoa(port)
-	c, err := net.Dial("tcp", dialadress)
-	if err != nil {
-		fmt.Println(cmd.Red, err, cmd.Reset)
-		exit(r)
-	}
-	s := bufio.NewScanner(c)
-	s.Split(N.ScanCRLF)
-	buf := make([]byte, 0, 64*1024)
-	s.Buffer(buf, 1024*1024)
-	var f bool
-	for s.Scan() {
-		if f {
-			signal <- struct{}{}
-			fmt.Print(clearLine)
-			fmt.Print(cursorVisible)
-		}
-		f = true
-		fmt.Println(s.Text())
-		if s.Text() == "exited." {
-			return
-		}
-		var (
-			err error
-			l   string
-		)
-		for {
-			fmt.Print(cmd.Cyan, cmd.Cursor, cmd.Reset)
-			l, err = r.ReadString('\n')
-			if err != nil {
-				panic(err)
-			}
-			l = string(N.DropCR([]byte(l[:len(l)-1])))
-			if l != "cls" && l != "clear" {
-				break
-			}
-			var cmd *exec.Cmd
-			if runtime.GOOS == "windows" {
-				cmd = exec.Command("cmd", "/c", "cls")
-			} else {
-				cmd = exec.Command("clear")
-			}
-			cmd.Stdout = os.Stdout
-			cmd.Run()
-		}
-		c.Write([]byte(l + N.CRLF))
-		signal <- struct{}{}
-	}
-	if f {
-		signal <- struct{}{}
-	}
-	fmt.Println(cmd.Red, s.Err(), cmd.Reset)
+const (
+	Add cTyp = iota
+	AddFeed
+	Remove
+	Ls
+	LsItems
+	Status
+	Stop
+)
+
+type Cmd struct {
+	Cmd cTyp            `json:"cmd"`
+	Arg string          `json:"arg"`
+	Raw json.RawMessage `json:"raw"`
 }
 
-func exit(r *bufio.Reader) {
-	r.ReadString('\n')
-	os.Exit(1)
+func Send(dialAddress string, cmd Cmd) (string, error) {
+	signal := make(chan struct{})
+	go waitProgress(signal)
+	c, err := net.Dial("tcp", dialAddress)
+	if err != nil {
+		return "", err
+	}
+	nc := &N.Conn{
+		TcpConn: c,
+	}
+	b, _ := json.Marshal(cmd)
+	err = nc.Write(string(b))
+	if err != nil {
+		return "", err
+	}
+	signal <- struct{}{}
+	read, err := nc.Read()
+	if err != nil {
+		return "", err
+	}
+	signal <- struct{}{}
+	return read, nil
 }
